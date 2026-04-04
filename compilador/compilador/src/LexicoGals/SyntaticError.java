f90aaa8e08eb6139ab936390aa453de3d9c02997
gustavoWAntunes <gustavowantunes@gmail.com>
2026-04-04 12:54:49 -0300

    trabalho 2

---
+++ compilador/compilador/src/LexicoGals/SyntaticError.java
@@ -0,0 +1,14 @@
+package LexicoGals;
+
+public class SyntaticError extends AnalysisError
+{
+    public SyntaticError(String msg, int position)
+	 {
+        super(msg, position);
+    }
+
+    public SyntaticError(String msg)
+    {
+        super(msg);
+    }
+}
